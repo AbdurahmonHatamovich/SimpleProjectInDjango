de41a35aeb4c325e0ae58bd5ebdb865d63d2329e
ABU <muruvat@gmail.com>
2024-04-02 21:48:45 +0500

    Davomi

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Adress,Students
+
+admin.site.register(Adress)
+admin.site.register(Students)
--- student/models.py
+++ student/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Adress(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Students(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField(default=18)
+    address = models.ForeignKey(Adress, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
